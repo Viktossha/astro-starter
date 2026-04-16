d03504a3b21ae22668ba154f915a30be7effdee9
Vika <vikapetrova8@gmail.com>
2026-04-16 23:21:12 +0300

    add preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cute-belekoy-a1da5e.netlify.app/"
+  site: "https://cute-belekoy-a1da5e.netlify.app/",
+  integrations: [preact()]
 });
