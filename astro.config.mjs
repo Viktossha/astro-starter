ab7eb8022c10a589bdbda8c63835107eb6e85d7c
Vika <vikapetrova8@gmail.com>
2026-04-16 23:12:06 +0300

    add rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://cute-belekoy-a1da5e.netlify.app/"
+});
